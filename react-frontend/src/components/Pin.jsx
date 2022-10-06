2e9c2960f71a8c2093dee1d1ad0b36533c3bc498
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-06 20:04:25 +0530

    pin detail component done

--- react-frontend/src/components/Pin.jsx
+++ react-frontend/src/components/Pin.jsx
@@ -102,13 +102,13 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                 <a
                   href={destination}
                   target="_blank"
-                  rel="noreferrer"rel="noreferrer"
+                  rel="noreferrer"
                   className="bg-white flex items-center gap-2 text-black font-bold p-2 px-4 rounded-full opacity-70 hover:opactiy-100 hover:shadow-md"
                 >
                   <BsFillArrowUpRightCircleFill />
-                  {destination.length > 20
-                    ? destination.slice(8, 20)
-                    : destination.slice(8)}
+                  {destination.length > 15
+                    ? `${destination.slice(0, 15)}...`
+                    : destination}
                 </a>
               )}
               {postedBy?._id === user.googleId && (
@@ -128,7 +128,7 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
         )}
       </div>
       <Link
-        to={`user-profile/${user?._id}`}
+        to={`user-profile/${postedBy?._id}`}
         className="flex gap-2 mt-2 items-center"
       >
         <img
